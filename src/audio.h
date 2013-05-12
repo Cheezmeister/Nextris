d35bcafa499a3ef5e3fc6680bcbf917e530e8e8d
Cheezmeister <cheezmeister@bob-vi>
2013-05-12 17:20:30 -0500

    Allow muting

--- src/audio.h
+++ src/audio.h
@@ -3,6 +3,7 @@ namespace nextris
     namespace audio
     {
         void init();
+        void enable(bool on = true);
         float noise(float mean = 0.0, float variance = 1.0);
         void play_sound(int color, int column, int row, int what);
         void update_bassline(unsigned long score);
